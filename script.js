fabf121c569ac6901fc56e192740199fc11cca43
LALITHOLAY <130987029+LALITHOLAY@users.noreply.github.com>
2023-07-27 17:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Function to update the width and height values in the h1 tag
+function updateSizeInfo() {
+  const widthValueSpan = document.getElementById('widthValue');
+  const heightValueSpan = document.getElementById('heightValue');
+
+  widthValueSpan.textContent = window.innerWidth;
+  heightValueSpan.textContent = window.innerHeight;
+}
+
+// Initial update when the page loads
+window.onload = function() {
+  updateSizeInfo();
+};
+
+// Update the size info whenever the window is resized
+window.addEventListener('resize', updateSizeInfo);
